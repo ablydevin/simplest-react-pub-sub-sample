ec933371195aec745c2d542ab93a1b30ed5f5c01
ablydevin <136481955+ablydevin@users.noreply.github.com>
2023-09-26 11:40:24 -0400

    Strreamlined.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
 import { Realtime }  from 'ably'
 import { AblyProvider, useChannel } from 'ably/react'
 
-function App() {
-  
+function App() {  
   const client = new Realtime({'authUrl':'[YOUR_ABLY_AUTH_URL]'});
   return (
-    <>
-      <AblyProvider client={client}>
-        <Messages />
-      </AblyProvider>
-    </>
+    <AblyProvider client={client}>
+      <Messages />
+    </AblyProvider>
   )
 }
 
 function Messages() {
-
   useChannel('inbound-messages', (message) => {
     console.log(message)
   });
-
-  return (
-    <div>Open your browser tools console to view incoming Ably messages</div>
-  )
+  return (<div>Open your browser tools console to view incoming Ably messages</div>)
 }
 
 export default App
